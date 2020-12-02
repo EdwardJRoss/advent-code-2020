d60682d7e7bf2333c7410066924cdd432c83407f
Edward Ross <edward@skeptric.com>
2020-12-02 20:53:35 +1100

    Advent of code day 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Find the two entries that sum to 2020 and then multiply those two numbers together.
+
+Usage - if the numbers are in a file called day1.txt:
+   ./day1.py <day1.txt
+"""
+import fileinput
+
+numbers = []
+for line in fileinput.input():
+    numbers.append(int(line.strip()))
+
+# Brute force search
+# O(n^2) ~ 400 checks
+for n1 in numbers:
+    for n2 in numbers:
+        if n1 < n2 and n1 + n2 == 2020:
+            print(n1 * n2)
